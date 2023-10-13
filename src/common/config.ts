39f006145a6fd596f3cbb6844a882e1e5f584786
Alejandro Rizzo <alerizzo@gmail.com>
2023-10-13 17:46:19 +0100

    fix: secrets will now be kept between projects ALA-634 (#15)

--- src/common/config.ts
+++ src/common/config.ts
@@ -1,4 +1,5 @@
 import * as vscode from 'vscode'
+import Logger from './logger'
 
 export class Config {
   private _wsConfig: vscode.WorkspaceConfiguration | undefined
@@ -14,33 +15,37 @@ export class Config {
   }
 
   public static async init(context: vscode.ExtensionContext) {
-    this._instance = new Config(context.secrets)
+    Config._instance = new Config(context.secrets)
 
-    const res = await this._instance?._secretStorage.get('codacy.apiToken')
-    this._instance._apiToken = res
+    const res = await Config._instance?._secretStorage.get('codacy.apiToken')
+    Config._instance._apiToken = res
 
-    return this._instance
+    if (Config._instance._apiToken) Logger.appendLine('Codacy API token found')
+
+    return Config._instance
   }
 
   public static async storeApiToken(value: string | undefined) {
-    if (!this._instance) return
+    if (!Config._instance) return
 
-    this._instance._apiToken = value
+    Config._instance._apiToken = value
 
     if (value) {
-      await this._instance._secretStorage.store('codacy.apiToken', value)
+      if (Config._instance._apiToken) Logger.appendLine('Storing API token...')
+      await Config._instance._secretStorage.store('codacy.apiToken', value)
     } else {
-      await this._instance._secretStorage.delete('codacy.apiToken')
+      if (Config._instance._apiToken) Logger.appendLine('Removing API token...')
+      await Config._instance._secretStorage.delete('codacy.apiToken')
     }
 
-    this._onDidConfigChange.fire(this._instance)
+    this._onDidConfigChange.fire(Config._instance)
   }
 
   public static get baseUri(): string {
-    return this._instance?._wsConfig?.get('baseUri') || 'https://app.codacy.com'
+    return Config._instance?._wsConfig?.get('baseUri') || 'https://app.codacy.com'
   }
 
   public static get apiToken(): string | undefined {
-    return this._instance?._apiToken
+    return Config._instance?._apiToken
   }
 }
