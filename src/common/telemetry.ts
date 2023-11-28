95726f12a93847a8fbd0b6fcd695cbea2b92afd2
Alejandro Rizzo <alerizzo@gmail.com>
2023-11-28 17:16:57 +0000

    clean: add telemetry (#20)
    * clean: add telemetry
    
    * clean: pick up Segment key from CI
    
    * clean: use standard field name
    
    * clean: fix release issues

---
+++ src/common/telemetry.ts
@@ -0,0 +1,44 @@
+import * as vscode from 'vscode'
+import { Analytics } from '@segment/analytics-node'
+import { EventProperties } from '@segment/analytics-core'
+import { User } from '../api/client'
+import { SEGMENT_WRITE_KEY } from '../env-secrets'
+
+class TelemetryClient {
+  private analytics: Analytics | undefined
+  private userId: string | undefined
+
+  constructor() {
+    if (SEGMENT_WRITE_KEY) {
+      this.analytics = new Analytics({ writeKey: SEGMENT_WRITE_KEY })
+    }
+  }
+
+  public identify(user: User) {
+    if (!vscode.env.isTelemetryEnabled || !this.analytics) return
+
+    this.userId = user.id.toString()
+
+    this.analytics.identify({
+      userId: this.userId,
+      traits: {
+        createdAt: user.created,
+      },
+    })
+  }
+
+  public track(event: string, properties: EventProperties) {
+    if (!vscode.env.isTelemetryEnabled || !this.analytics) return
+
+    if (this.userId) {
+      this.analytics.track({
+        userId: this.userId,
+        event,
+        properties,
+      })
+    }
+  }
+}
+
+const Telemetry = new TelemetryClient()
+export default Telemetry
