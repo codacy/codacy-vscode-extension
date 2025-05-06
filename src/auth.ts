6f582caeae194154027a043fa35bacde6a5025c5
Neda Kaighobadi <neda.kaighobadi@gmail.com>
2025-05-06 11:16:13 +0300

    send repo params when connecting

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,7 @@
 import * as vscode from 'vscode'
 import { Config } from './common/config'
+import { parseGitRemote } from './common/parseGitRemote'
+import Logger from './common/logger'
 
 export class AuthUriHandler extends vscode.EventEmitter<vscode.Uri> implements vscode.UriHandler {
   public handleUri(uri: vscode.Uri): void {
@@ -31,7 +33,25 @@ export const detectEditor = (): IDE => {
 }
 
 export const signIn = async () => {
+  const gitExtension = vscode.extensions.getExtension('vscode.git')?.exports
+  const api = gitExtension?.getAPI(1)
+
+  // Get the first open repository
+  const repository = api?.repositories[0]
+  let params = ''
+
+  if (repository) {
+    const remoteUrl = repository.state.remotes[0]?.fetchUrl || repository.state.remotes[0]?.pushUrl
+    if (remoteUrl) {
+      try {
+        const { provider, organization, repository: repoName } = parseGitRemote(remoteUrl)
+        params = `?provider=${provider}&organization=${organization}&repository=${repoName}`
+      } catch (e) {
+        Logger.error(`Error parsing git remote: ${e}`)
+      }
+    }
+  }
   const editor = detectEditor()
-  const uri = vscode.Uri.parse(`${Config.baseUri}/auth/${editor}`)
+  const uri = vscode.Uri.parse(`${Config.baseUri}/auth/${editor}${params}`)
   await vscode.env.openExternal(uri)
 }
