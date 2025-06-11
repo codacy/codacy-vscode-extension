aa7e87f4b7dac2b774fb206c1c9a1a4bfc804731
Nuno Fernandes <nuno.fernandes@codacy.com>
2025-06-11 18:20:52 +0100

    test: Adding one test for command registration

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,11 +1,27 @@
-import * as assert from 'assert'
 import * as vscode from 'vscode'
+import { MockExtensionContext } from '../mocks/MockExtensionContext'
+import * as sinon from 'sinon'
+import { CodacyCloud } from '../../git/CodacyCloud'
+import { CommandType, wrapExtensionCommand } from '../../common/utils'
+
+const testRegisterCommands = async (context: vscode.ExtensionContext, codacyCloud: CodacyCloud) => {
+  const commands: Record<string, CommandType> = {
+    'codacy.refresh': () => codacyCloud.refresh(),
+  }
+  Object.keys(commands).forEach((cmd) => {
+    context.subscriptions.push(vscode.commands.registerCommand(cmd, wrapExtensionCommand(commands[cmd], cmd)))
+  })
+}
 
 suite('Extension Test Suite', () => {
-  vscode.window.showInformationMessage('Start all tests.')
+  vscode.window.showInformationMessage('Start Extension Test Suite.')
 
-  test('Sample test', () => {
-    assert.strictEqual(-1, [1, 2, 3].indexOf(5))
-    assert.strictEqual(-1, [1, 2, 3].indexOf(0))
+  test('Command registration and execution', async () => {
+    const context = new MockExtensionContext()
+    const refreshStub = sinon.stub()
+    const codacyCloud = { refresh: refreshStub } as unknown as CodacyCloud
+    await testRegisterCommands(context, codacyCloud)
+    await vscode.commands.executeCommand('codacy.refresh')
+    sinon.assert.calledOnce(refreshStub)
   })
 })
